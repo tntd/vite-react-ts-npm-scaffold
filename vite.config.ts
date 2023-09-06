05a81ba33375760718dbd881c25b216d4ae96e7b
chunlong.ni <chunlong.ni@tongdun.net>
2023-09-06 14:37:44 +0800

    优化教授叫

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react';
 import typescript from '@rollup/plugin-typescript';
 import libCss from 'vite-plugin-libcss';
 
-
 function resolve(str: string) {
   return path.resolve(__dirname, str);
 }
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -34,12 +32,12 @@ export default defineConfig({
     cssCodeSplit:true,
     lib: {
       entry: resolve('src/index.ts'),
+      formats:['es','umd'],                       // formats  ['es','cjs','umd','iife']
       name: 'index',
       fileName: 'index',
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom'],     // 确保外部化处理那些你不想打包进库的依赖
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
