97eb5dc4b1db24e7d4b0ab58a759b73b1f150d8b
chunlong.ni <chunlong.ni@tongdun.net>
2023-09-06 10:25:02 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import typescript from '@rollup/plugin-typescript';
+
+function resolve(str: string) {
+  return path.resolve(__dirname, str);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    typescript({
+      target: 'es5',
+      rootDir: resolve('src/'),
+      declaration: true,
+      declarationDir: resolve('dist'),
+      exclude: resolve('node_modules/**'),
+      allowSyntheticDefaultImports: true,
+    }),
+  ],
+  server: {
+    port: 8001,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    // 防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制
+    cssTarget: 'chrome61',
+    lib: {
+      entry: resolve('src/index.ts'),
+      name: 'my-packages',
+      fileName: 'my-packages',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'react-dom'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'react',
+          'react-dom': 'react-dom',
+        },
+      },
+    },
+  },
+});
